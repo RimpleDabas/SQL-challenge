b38a5e52fe294f7af62efebc9baa46ed16679af4
RimpleDabas <125090606+RimpleDabas@users.noreply.github.com>
2023-04-19 15:51:36 -0400

    query_file added

--- EmployeeSQL/data/employee_schema.sql
+++ EmployeeSQL/data/employee_schema.sql
@@ -2,25 +2,25 @@
 
 
 Create table titles(
-   					title_id Varchar primary key,
-					title Varchar not null);
+   					title_id Varchar(5) primary key,
+					title Varchar(30) not null);
 					
 
 
 Create table departments(
-						dept_no varchar primary key,
-						dept_name varchar);
+						dept_no varchar(5) primary key,
+						dept_name varchar(30) not null);
 
 
 
 Create table employees(
-					emp_no int primary key,
-					emp_title_id varchar not null,
-					birth_date varchar not null,
-					first_name varchar not null,
-					last_name varchar not null,
-					sex varchar not null,
-					hire_date varchar not null,
+					emp_no int(10) primary key,
+					emp_title_id varchar(10) not null,
+					birth_date date,
+					first_name varchar(30) not null,
+					last_name varchar(30) not null,
+					sex varchar(1) not null,
+					hire_date date,
 					foreign key(emp_title_id) references titles(title_id)
 					);
 					
@@ -28,8 +28,8 @@ Create table employees(
 
 
 Create table dept_emp(
-					emp_no int not null,
-					dept_no varchar not null,
+					emp_no int(5) not null,
+					dept_no varchar(30) not null,
 					primary key(emp_no, dept_no),
                     foreign key (emp_no) references employees(emp_no),
 					foreign key (dept_no) references departments(dept_no)
@@ -40,15 +40,15 @@ Create table dept_emp(
 
 
 Create table salaries (
-					emp_no int primary key,
-					salary int not null,
+					emp_no int(6) primary key,
+					salary int(10) not null,
 					foreign key(emp_no) references employees(emp_no)
 					);
 					
 
 Create table dept_manager(
-						dept_no varchar not null,
-						emp_no int not null,
+						dept_no varchar(10) not null,
+						emp_no int(6) not null,
 						primary key(dept_no,emp_no),
 						foreign key (dept_no) references departments (dept_no),
 						foreign key (emp_no) references employees (emp_no)
